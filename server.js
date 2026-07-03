cd33604d331fcbfee80b1908bfee0a51bb519981
amulya <24wh1a05w8@bvrithyderabad.edu.in>
2026-07-03 14:09:06 +0530

    update

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = 3002;
+const PORT = process.env.PORT || 3002;
+//webserver
+app.use(express.static('frontend'));
+app.listen(PORT,functison(){
+    console.log("Successfully running at http://localhost:"+PORT);
+});
